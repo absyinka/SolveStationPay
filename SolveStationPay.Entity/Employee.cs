2003a58cf2f6ee1d257d6335c7888cb2f5532d15
absyinka <absyinka@gmail.com>
2020-02-06 23:07:16 +0100

    Create and Edit - EmployeeController

--- SolveStationPay.Entity/Employee.cs
+++ SolveStationPay.Entity/Employee.cs
@@ -20,6 +20,7 @@ namespace SolveStationPay.Entity
         public string ImageUrl { get; set; }
         public DateTime DOB { get; set; }
         public DateTime DateJoined { get; set; }
+        public string Phone { get; set; }
         public string Designation { get; set; }
         public string Email { get; set; }
         [Required, MaxLength(50)]
